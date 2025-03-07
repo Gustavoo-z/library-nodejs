42fbbfc08bb1d8399275f3ede0b52eff68acf6e0
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-03-07 10:32:29 -0300

    Add functions

--- src/index.js
+++ src/index.js
@@ -4,13 +4,30 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf8', (erro, texto) => {
-    verificaPalavrasDuplicadas(texto);
+    quebraEmParagrafos(texto);
+    // verificaPalavrasDuplicadas(texto);
 })
+
+function quebraEmParagrafos(texto) {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.flatMap((paragrafo) => {
+        if(!paragrafo) return [];
+        return verificaPalavrasDuplicadas(paragrafo);
+    })
+    console.log(contagem);
+}
+
+function limpaPalavras(palavra) {
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
 
 function verificaPalavrasDuplicadas(texto) {
     const listaPalavras = texto.split(' ');
     const resultado = {};
     listaPalavras.forEach(palavra => {
-        resultado[palavra] = (resultado[palavra] || 0) + 1;})
-    console.log(resultado);
+        if(palavra.length >= 3) {
+            const palavraLimpa = limpaPalavras(palavra);
+        resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;}
+        })
+    return resultado;
     }
