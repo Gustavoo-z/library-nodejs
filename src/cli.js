ba71b5f5c5da685ed4085955049d32cb6e6525f1
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-03-09 11:47:20 -0300

    Save archive

--- src/cli.js
+++ src/cli.js
@@ -4,12 +4,25 @@ import { contaPalavras } from './index.js';
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
+const endereco = caminhoArquivo[3];
 
 fs.readFile(link, 'utf8', (erro, texto) => {
     try {
         if (erro) throw erro;
-        contaPalavras(texto);
+        const resultado = contaPalavras(texto);
+        criaESalvaArquivo(resultado, endereco);
     } catch (erro) {
         trataErros(console.log(erro))
     }
 });
+
+async function criaESalvaArquivo(listaPalavras, endereco) {
+    const arquivoNovo = `${endereco}/resultado.txt`;
+    const textoPalavras = JSON.stringify(listaPalavras);
+    try {
+        await fs.promises.writeFile(arquivoNovo, textoPalavras);
+        console.log('Arquivo salvo com sucesso!');
+    } catch (erro) {
+        throw erro;
+    }
+}
