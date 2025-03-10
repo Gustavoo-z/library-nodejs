d1e2e77c478f0a9920a482630f2851dc3f42afbd
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-03-10 15:18:11 -0300

    Add functions

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
+import { montaSaidaArquivo } from './helpers.js';
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
@@ -16,13 +17,27 @@ fs.readFile(link, 'utf8', (erro, texto) => {
     }
 });
 
-async function criaESalvaArquivo(listaPalavras, endereco) {
+// async function criaESalvaArquivo(listaPalavras, endereco) {
+//     const arquivoNovo = `${endereco}/resultado.txt`;
+//     const textoPalavras = JSON.stringify(listaPalavras);
+//     try {
+//         await fs.promises.writeFile(arquivoNovo, textoPalavras);
+//         console.log('Arquivo salvo com sucesso!');
+//     } catch (erro) {
+//         throw erro;
+//     }
+// }
+
+function criaESalvaArquivo(listaPalavras, endereco) {
     const arquivoNovo = `${endereco}/resultado.txt`;
-    const textoPalavras = JSON.stringify(listaPalavras);
-    try {
-        await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    const textoPalavras = montaSaidaArquivo(listaPalavras);
+        
+    fs.promises.writeFile(arquivoNovo, textoPalavras)
+    .then(() => {
         console.log('Arquivo salvo com sucesso!');
-    } catch (erro) {
+    })
+    .catch((erro) => {
         throw erro;
-    }
+    })
+    .finally(() => console.log('Processo finalizado!'))
 }
